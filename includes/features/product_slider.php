7b600c66d164a2ae5b249b38e4c81840aa79b57c
Philip Krueck <philip.krueck@apploft.de>
2019-05-10 17:57:09 +0200

    added connection to css sheet

--- includes/features/product_slider.php
+++ includes/features/product_slider.php
@@ -1,34 +1,54 @@
-<!-- CONTENT-AREA -->
+<!-- PHP-AREA -->
+<?php
 
-<div id="mi-slider" class="mi-slider">
-	<ul>
-		<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img01"><h4>Neueste 1</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img02"><h4>Neueste 2</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img03"><h4>Neueste 3</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_4.jpg" alt="img04"><h4>Neueste 4</h4></a></li>
-	</ul>
-	<ul>
-		<li><a href="#"><img src="/temporary_assets/imm_5.jpg" alt="img05"><h4>Hamburg 1</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_6.jpg" alt="img06"><h4>Hamgurg 2</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img07"><h4>Hamburg 3</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img08"><h4>Hamburg 4</h4></a></li>
-	</ul>
-	<ul>
-		<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img09"><h4>Berlin 1</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_4.jpg" alt="img10"><h4>Berlin 2</h4></a></li>
-        <li><a href="#"><img src="/temporary_assets/imm_5.jpg" alt="img11"><h4>Berlin 3</h4></a></li>
-        <li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img11"><h4>Berlin 4</h4></a></li>
-	</ul>
-	<ul>
-		<li><a href="#"><img src="/temporary_assets/imm_6.jpg" alt="img12"><h4>München 1</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img13"><h4>München 2</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img14"><h4>München 3</h4></a></li>
-		<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img15"><h4>München 4</h4></a></li>
-	</ul>
-	<nav>
-		<a href="#">Neueste</a>
-		<a href="#">Hamburg</a>
-		<a href="#">Berlin</a>
-		<a href="#">München</a>
-	</nav>
-</div>
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <!-- HEAD-AREA -->
+    <head>
+
+        <!-- Link-Relations -->
+        <link rel="stylesheet" href="/css/features/product_slider.css">
+
+    </head>
+
+    <!-- CONTENT-AREA -->
+
+	<h2>My Slider</h2>
+    <body>
+		<div id="slider" class="slider">
+			<ul>
+				<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img01"><h4>Neueste 1</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img02"><h4>Neueste 2</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img03"><h4>Neueste 3</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_4.jpg" alt="img04"><h4>Neueste 4</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="/temporary_assets/imm_5.jpg" alt="img05"><h4>Hamburg 1</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_6.jpg" alt="img06"><h4>Hamgurg 2</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img07"><h4>Hamburg 3</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img08"><h4>Hamburg 4</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img09"><h4>Berlin 1</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_4.jpg" alt="img10"><h4>Berlin 2</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_5.jpg" alt="img11"><h4>Berlin 3</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img11"><h4>Berlin 4</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="/temporary_assets/imm_6.jpg" alt="img12"><h4>München 1</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_1.jpg" alt="img13"><h4>München 2</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_2.jpg" alt="img14"><h4>München 3</h4></a></li>
+				<li><a href="#"><img src="/temporary_assets/imm_3.jpg" alt="img15"><h4>München 4</h4></a></li>
+			</ul>
+			<nav>
+				<a href="#">Neueste</a>
+				<a href="#">Hamburg</a>
+				<a href="#">Berlin</a>
+				<a href="#">München</a>
+			</nav>
+		</div>
+	</body>
+</html>
