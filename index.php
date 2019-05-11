f192427c82f62f3471cd58daad6ee722d3dfdc4c
Philip Krueck <philip.krueck@apploft.de>
2019-05-11 13:13:42 +0200

    added homepage file back

--- index.php
+++ index.php
@@ -1,69 +1,132 @@
+<!-- PHP-AREA -->
+<?php
+
+?>
+
 <!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>Simple Multi-Item Slider with CSS Animations and jQuery</title>
-		<meta name="description" content="Simple Multi-Item Slider: Category slider with CSS animations" />
-		<meta name="keywords" content="jquery plugin, item slider, categories, apple slider, css animation" />
-		<meta name="author" content="Codrops" />
-		<link rel="shortcut icon" href="../favicon.ico"> 
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		<script src="js/modernizr.custom.63321.js"></script>
-	</head>
-	<body>
-		<div class="container">	
-			<!-- Codrops top bar -->
-			<div class="codrops-top clearfix">
-				<a href="http://tympanus.net/Development/SliderPagination/"><strong>&laquo; Previous Demo: </strong>Slider Pagination Concept</a>
-				<span class="right"><a href="http://tympanus.net/codrops/?p=13218"><strong>Back to the Codrops Article</strong></a></span>
-			</div><!--/ Codrops top bar -->
-			<header class="clearfix">
-				<h1>Simple Multi-Item Slider <span>Category slider with CSS animations</span></h1>
-			</header>
-			<div class="main">
-				<div id="mi-slider" class="mi-slider">
-					<ul>
-						<li><a href="#"><img src="images/1.jpg" alt="img01"><h4>Boots</h4></a></li>
-						<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>Oxfords</h4></a></li>
-						<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
-						<li><a href="#"><img src="images/4.jpg" alt="img04"><h4>Sneakers</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
-						<li><a href="#"><img src="images/6.jpg" alt="img06"><h4>Hats &amp; Caps</h4></a></li>
-						<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
-						<li><a href="#"><img src="images/8.jpg" alt="img08"><h4>Scarves</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
-						<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
-						<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
-						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
-						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
-						<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
-					</ul>
-					<nav>
-						<a href="#">Shoes</a>
-						<a href="#">Accessories</a>
-						<a href="#">Watches</a>
-						<a href="#">Bags</a>
-					</nav>
-				</div>
-			</div>
-		</div><!-- /container -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="js/jquery.catslider.js"></script>
-		<script>
-			$(function() {
+<html>
 
-				$( '#mi-slider' ).catslider();
+    <!-- HEAD-AREA -->
+    <head>
+    
+        <!-- Homepage-Title -->
+        <title>Page Title</title>
 
-			});
-		</script>
-	</body>
-</html>
+        <!-- Link-Relations -->
+        <link rel="stylesheet" href="css/styles.css">
+
+    </head>
+
+    <!-- BODY-AREA -->
+    <body>
+
+        <!-- HEADER-AREA -->
+        <header>
+            <?php 
+                include ('includes/header.php');
+            ?>
+        </header>
+
+        <!-- MAIN-AREA -->
+        <main>
+
+            <!-- BANNER-AREA -->
+            <div class="banner">
+
+                <!-- PLACEHOLDER -->
+                <h2 style="
+                    text-align: center;
+                    padding-top: 100px;
+                    margin: 0px;"
+                >BANNER</h2>
+
+            </div>
+
+            <!-- CONTENT-AREA -->
+            <div class="content">
+            
+                <!-- SEARCH-AREA -->
+                <div class="search-area">
+
+                    <!-- PLACEHOLDER -->
+                    <h2 style="
+                    text-align: center;
+                    margin: 0px;"
+                    >SEARCH</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                     et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                     Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+                     diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                     At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                     takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                </div>
+
+                <!-- RESULTS-AREA -->
+                <div class="results-area">
+
+                    <!-- PLACEHOLDER -->
+                    <h2 style="
+                    text-align: center;
+                    margin: 0px;"
+                    >RESULTS</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                     et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                     Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+                     diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                     At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                     takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                </div>
+
+                <!-- PROMOTED-AREA -->
+                <div class="promoted-area">
+                
+                    <!-- PLACEHOLDER -->
+                    <h2 style="
+                    text-align: center;
+                    margin: 0px;"
+                    >PROMOTED</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                     et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                     Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+                     diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                     At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                     takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                </div>
+
+                <!-- NEWEST-AREA -->
+                <div class="newest-area">
+                
+                    <!-- PLACEHOLDER -->
+                    <h2 style="
+                    text-align: center;
+                    margin: 0px;"
+                    >NEWEST</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                     et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                     Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+                     diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                     At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                     takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                </div>
+
+            </div>
+        </main>
+
+        <!-- FOOTER-AREA -->
+        <footer>
+            <?php
+                include ('includes/footer.php');
+            ?>
+        </footer>
+    
+    </body>
+
+</html> 
