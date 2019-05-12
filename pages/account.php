151a8b854433e2c038106769205a20a5eabf85a4
Oliver <oliver@buettner-hh.de>
2019-05-12 20:04:20 +0200

    created all subpages

---
+++ pages/account.php
@@ -0,0 +1,68 @@
+<!-- PHP-AREA -->
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <!-- HEAD-AREA -->
+    <head>
+    
+        <!-- Homepage-Title -->
+        <title>Page Title</title>
+
+        <!-- Link-Relations -->
+        <link rel="stylesheet" href="../css/styles.css">
+
+    </head>
+
+    <!-- BODY-AREA -->
+    <body>
+
+        <!-- HEADER-AREA -->
+        <header>
+            <?php 
+                include ('../includes/header.php');
+            ?>
+        </header>
+
+        <!-- MAIN-AREA -->
+        <main>
+
+            <!-- BANNER-AREA -->
+            <div class="banner">
+
+                <!-- PLACEHOLDER -->
+                <h2 style="
+                    text-align: center;
+                    padding-top: 100px;
+                    margin: 0px;"
+                >MARKLER XY</h2>
+
+            </div>
+
+            <!-- CONTENT-AREA -->
+            <div class="content">          
+
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+            </div>
+        </main>
+
+        <!-- FOOTER-AREA -->
+        <footer>
+            <?php
+                include ('../includes/footer.php');
+            ?>
+        </footer>
+    
+    </body>
+
+</html> 
