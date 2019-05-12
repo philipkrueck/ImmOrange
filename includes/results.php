18420c8c9c50aec43f679f3261f9978528eb3cae
Oliver <oliver@buettner-hh.de>
2019-05-12 19:43:09 +0200

    exported results to separate file to be used in several pages

---
+++ includes/results.php
@@ -0,0 +1,22 @@
+<!-- PLACEHOLDER -->
+<h2 style="
+    text-align: center;
+    margin: 0px;"
+    >RESULTS</h2>
+    
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+        takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+    <?php
+        echo "<h2 style='
+        text-align: center;
+        margin: 0px;'
+        >POST</h2>";
+        print_r($_POST);
+        echo "</pre>";
+    ?>
