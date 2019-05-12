a4adb50f0a420c542736462fefb297e1659ed63f
Oliver <oliver@buettner-hh.de>
2019-05-12 16:57:06 +0200

    created homepage-search-form

---
+++ includes/features/price_range.php
@@ -0,0 +1,17 @@
+<script>
+  $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 4000,
+      values: [ 700, 3300],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $("#price_min").val(ui.values[ 0 ]);
+        $("#price_max").val(ui.values[ 1 ]);
+      }
+    });
+    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + " €" + 
+      "   -   " + $( "#slider-range" ).slider( "values", 1 ) + " €" );
+  } );
+  </script>
