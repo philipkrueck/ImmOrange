56a303c7909163ec6f50b2173cf2ef4ca3accc77
Leo von Schmeling-D <>
2019-05-16 13:53:06 +0200

    initial commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,34 @@
-<!-- PLACEHOLDER -->
-<h2 style="
-    text-align: center;
-    padding-top: 80px;
-    margin: 0px;"
->FOOTER</h2>
+
+<!-- FOOTER -->
+    <div style="  background-color: rgba(0, 101, 114, 0.65);width:100%;height:300px;">
+
+      <div class="smallfooter">
+     <h4>  Entdecke unsere Inserate:</h4>
+     <p style="text-align: center; margin-left: 0px"> Häuser:  <img class="iconHouse" src="../img/icons/House.png"></p>
+     <p style="text-align: center; margin-left: -32px"> Wohnungen:   <img class="iconHouse" src="../img/icons/Apartement.png"></p>
+     <p style="text-align: center; margin-left: 13px"> Suche:  <img class="iconHouse" src="../img/icons/Lupe.png" ></p>
+
+      </div>
+
+      <div class="smallfooter">
+
+      <h3>Finde uns auf:</h3>
+        <a href="https://www.facebook.com/"><img class="iconTIF" src="../img/icons/Facebook.png"></a>
+        <a href="https://twitter.com/?lang=de"><img class="iconTIF" src="../img/icons/Twitter.png" ></a>
+          <a href="https://www.instagram.com/?hl=de"><img class="iconTIF" src="../img/icons/Instagram.png" ></a>
+      </div>
+
+      <div class="smallfooter">
+
+      <h4>Kontaktiere die Entwickler:</h4>
+      <p style="text-align: center; margin-left: 5px">  Philip: <img class="iconWP" src="../img/icons/Whatsapp.png" ></p>
+      <p style="text-align: center; margin-left: 0px">  Olliver: <img class="iconWP" src="../img/icons/Whatsapp.png" ></p>
+      <p style="text-align: center; margin-left: 0px">  Marcel: <img class="iconWP" src="../img/icons/Whatsapp.png" ></p>
+      <p style="text-align: center; margin-left: 21px">  Leo: <img class="iconWP" src="../img/icons/Whatsapp.png" ></p>
+     </div>
+
+    </div>
+    <div class="minifooter">
+       ©Imm<span style="color:orange;">Orange</span>
+    </div>
+    <!-- END FOOTER -->
--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,18 @@
+<!-- HEADER -->
+  <h1 class="logo">
+      <span>Imm</span><span>Orange</span>
+  </h1>
+  
+  <div class="headerDIV">
+    <img class="iconUSER" src="../img/icons/Benutzer.png" >
+  </div>
 
-<!-- PLACEHOLDER -->
-<a href="/" style="text-decoration: none;"><h1 style="
-    margin: 0px;
-    text-align: center;
-    background-color: #d18800;
-    border-radius: 50px;
-    padding: 15px;
-    width: 200px;
-    font-size: 20px;
-    font-weight: bold;
-    margin: 14px 0px 0px 15px;
-    border: 4px dashed #ffa500;
-    text-decoration: none;
-    color: white;">
-    <span>Imm</span><span>Orange</span>
-</h1></a>
+
+
+
+
+
+
+
+
+<!-- END HEADER -->
