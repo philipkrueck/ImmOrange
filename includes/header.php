02ef1fd1311c66aa567169384fa00e2754200d0f
Leo von Schmeling-D <>
2019-05-17 12:41:23 +0200

    navbar

--- includes/header.php
+++ includes/header.php
@@ -3,18 +3,11 @@
       <span>Imm</span><span>Orange</span>
   </h1>
 
-  <div class="headerDIV";class="dropdown";class="dropdown-content">
-    <img class="iconUSER" src="../img/icons/bathtub.png" >
-  </div>
+    <img class="iconUSER" src="../img/icons/Benutzer.png" >
 
-  <div class="dropdown">
-  <span>Mouse over me</span>
-  <div class="dropdown-content">
-    <p>Hello World!</p>
-  </div>
+  
+
+<div >
+<input type="text" name="suche" value=""; class="navsearch";>
 </div>
-
-
-
-
 <!-- END HEADER -->
