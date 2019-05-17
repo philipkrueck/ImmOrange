6a2cf4b87884de431aa4e3ab0b0a2d74f2ba696a
Leo von Schmeling-D <>
2019-05-17 10:36:22 +0200

    merged master to header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,20 @@
 <!-- HEADER -->
+  <h1 class="logo">
+      <span>Imm</span><span>Orange</span>
+  </h1>
+
+  <div class="headerDIV";class="dropdown";class="dropdown-content">
+    <img class="iconUSER" src="../img/icons/bathtub.png" >
+  </div>
+
+  <div class="dropdown">
+  <span>Mouse over me</span>
+  <div class="dropdown-content">
+    <p>Hello World!</p>
+  </div>
+</div>
+
+
+
 
 <!-- END HEADER -->
