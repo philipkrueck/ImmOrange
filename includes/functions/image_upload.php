bf30e8cde2cc4f92359ca7f65ac23606c42e4786
Philip Krueck <philip.krueck@apploft.de>
2019-05-17 11:43:53 +0200

    created file for js

---
+++ includes/functions/image_upload.php
@@ -0,0 +1,12 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js">
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#profile-img-tag').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
