4b15f85aa5ed40fd9d44913ab40fae26a4e48cd0
Leo von Schmeling-D <>
2019-05-17 14:23:16 +0200

    headerwork

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,26 @@
 <!-- HEADER -->
+<!--LOGO-->
   <h1 class="logo">
       <span>Imm</span><span>Orange</span>
   </h1>
+  <!--END LOGO-->
+  <!--DROPDOWN-->
+<div class="dropdown">
+  <img class="iconUSER" src="../img/icons/Benutzer.png" >
+  <div class="iconUSER-content">
+    <p>Regestrieren</p>
+    <p>Login</p>
+    <p>Accountinformation</p>
+  </div>
+</div>
+<!--END DROPDOWN-->
 
-    <img class="iconUSER" src="../img/icons/Benutzer.png" >
 
-  
 
+<!--SUCHE-->
 <div >
 <input type="text" name="suche" value=""; class="navsearch";>
 </div>
+<!--END SUCHE-->
+
 <!-- END HEADER -->
