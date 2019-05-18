330f617d7b813a34860e55c2e91685a6534bd956
Philip Krueck <philip.krueck@apploft.de>
2019-05-18 14:41:20 +0200

    added mockdata

--- project.sql
+++ project.sql
@@ -59,3 +59,85 @@ CREATE TABLE favorite_offer (
   FOREIGN KEY(offer_id) REFERENCES property_offer(offer_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(acc_email) REFERENCES account(acc_email) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+INSERT INTO realtor (realtor_id, company_name, tel_number) VALUES
+(1, 'Brookfield Asset Management', '01(54)807-34-24'),
+(2, 'American Tower Corporation', '01(54)807-34-24'),
+(3, 'Simon Property Group', '01(54)807-34-24'),
+(4, 'Weyerhaeuser', '01(54)807-34-24'),
+(5, 'Westfield', '01(54)807-34-24'),
+(6, 'AvalonBay Communities', '01(54)807-34-24'),
+(7, 'Jake Simmons Lake View Houses', '01(54)807-34-24');
+
+INSERT INTO account (acc_email, acc_password, first_name, last_name, realtor_id, creation_date) VALUES 
+('Franson@gmail.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Franson', 'Timothy', '1', CURRENT_TIMESTAMP), 
+('americantower@corporation.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Hamm', 'Jose', '2', CURRENT_TIMESTAMP), 
+('simon@propertygroup.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Jackson', 'Connie', '3', CURRENT_TIMESTAMP), 
+('sapp.weyerhaeuser@gmail.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Sapp', 'Vanessa', '4', CURRENT_TIMESTAMP), 
+('hunt.westfield@gmail.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Hunt', 'William', '5', CURRENT_TIMESTAMP), 
+('riordan@avalonbay.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Riordan', 'Linda', '6', CURRENT_TIMESTAMP), 
+('isaac@lakeview.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS','Isaac', 'Ruben', '7', CURRENT_TIMESTAMP), 
+('flemming@hotmail.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Flemming', 'Frank', '1', CURRENT_TIMESTAMP), 
+('hommes@myhsba.de', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Hommes', 'Megan', NULL, CURRENT_TIMESTAMP), 
+('long@gmail.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Long', 'Katherine', NULL, CURRENT_TIMESTAMP), 
+('hill@hillert.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Hill', 'Becky', NULL, CURRENT_TIMESTAMP), 
+('pinard@peanuts.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Pinard', 'Kevin', NULL, CURRENT_TIMESTAMP), 
+('grady@slimshady.com', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Grady', 'Matthew', NULL, CURRENT_TIMESTAMP), 
+('soto@sushikyoto.tk', '$2y$10$FJX1BetFa9sChYb8S3YHRe3nM7cz.bhtCEL7U2PqV9x9xrrStz/PS', 'Soto', 'Benjamin', NULL, CURRENT_TIMESTAMP);
+
+INSERT INTO property_offer (offer_id, offer_name, address_id, realtor_id, is_apartment, purchasing_type, rooms, price, qm, image_id, 
+has_garden, has_garage, has_bathtub, has_elevator, has_balcony, creation_date) VALUES 
+('1', 'Wohnung mit Aussicht **TOP LAGE**', '15', '1', '0', '0', '3', '200000', '100', NULL, '0', '0', '1', '0', '1', CURRENT_TIMESTAMP),
+ ('2', 'Direkt am Stadtpark!', '16', '2', '0', '0', '4', '500000', '60', NULL, '0', '1', '1', '0', '1', CURRENT_TIMESTAMP), 
+ ('3', 'Ubahn-Anbindung TOP', '17', '3', '1', '1', '2', '1500', '45', NULL, '0', '0', '0', '0', '1', CURRENT_TIMESTAMP), 
+ ('4', 'Naturparadies! Haustiere erlaubt.', '18', '4', '0', '1', '5', '2000', '120', NULL, '1', '1', '0', '0', '0', CURRENT_TIMESTAMP), 
+ ('5', 'Perfekt zum JOGGEN!', '19', '5', '1', '1', '2', '700', '40', NULL, '0', '0', '1', '1', '0', CURRENT_TIMESTAMP), 
+ ('6', '24-Stunden Tageslicht! 2-Z.-Wohn.', '20', '1', '6', '0', '4', '700', '70', NULL, '1', '1', '0', '1', '0', CURRENT_TIMESTAMP), 
+ ('7', 'Altbau, ruhig, Stadtpark-Nähe **WINTERHUDE**', '1', '7', '1', '1', '4', '900', '80', NULL, '0', '0', '1', '0', '1', CURRENT_TIMESTAMP), 
+ ('8', 'frisch RENOVIERTes City-App.', '2', '1', '1', '0', '5', '1400', '105', NULL, '0', '1', '1', '1', '1', CURRENT_TIMESTAMP), 
+ ('9', 'U1 & U3 !!! Perfekt für Singles.', '3', '2', '1', '1', '2', '600', '20', NULL, '0', '0', '0', '0', '0', CURRENT_TIMESTAMP), 
+ ('10', 'WG-ZIMMER * Shopping-Street zentral', '4', '3', '1', '1', '1', '300', '15', NULL, '0', '1', '0', '1', '1', CURRENT_TIMESTAMP), 
+ ('11', 'Mit Aussicht! Edel Altbau über Dönerbude', '5', '4', '0', '0', '3', '670', '65', NULL, '0', '1', '0', '1', '1', CURRENT_TIMESTAMP), 
+ ('12', 'Alpen in Sicht! Traumhüdli mit Kamin', '6', '5', '0', '0', '5', '800000', '120', NULL, '1', '1', '1', '0', '1', CURRENT_TIMESTAMP), 
+ ('13', 'Hauptstadt olé! Hipster-WG', '7', '6', '1', '1', '1', '400', '25', NULL, '0', '0', '1', '1', '1', CURRENT_TIMESTAMP), 
+ ('14', 'fast Hamburg! Landeshauptstadt Kiel', '8', '7', '0', '1', '4', '6000', '78', NULL, '1', '1', '1', '0', '1', CURRENT_TIMESTAMP);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
