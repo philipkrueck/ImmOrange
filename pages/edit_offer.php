a88e79802883f34e38e28001311cdf6e6ae80c66
Philip Krueck <philip.krueck@apploft.de>
2019-05-19 16:20:57 +0200

    show error message if not all fields filled out in edit page

--- pages/edit_offer.php
+++ pages/edit_offer.php
@@ -1,6 +1,60 @@
 <?php
     require_once('../includes/functions/pdo.php');
     require_once('../includes/functions/private.php');
+    include('../includes/functions/random_id.php');
+
+    if (isset($_POST['submit_offer'])) {
+        if (checkAllPostVariablesAreSet()) {
+            setSessionVariables();
+            updatePropertyOffer($_GET['offer_id']);
+            header("Location: offer.php?offer_id=".$rand_offer_id);
+            return;
+        } else {
+            header("Location: create_offer.php?not_filled"); 
+            return;
+        }
+    }
+
+    function updatePropertyOffer($offer_id) {
+        echo "update property offer";
+    }
+
+    function setSessionVariables() {
+        $_SESSION['offer_name'] = $_POST['offer_name'];
+        $_SESSION['number_of_rooms'] = $_POST['number_of_rooms'];
+        $_SESSION['qm'] = $_POST['qm'];
+        $_SESSION['price'] = $_POST['price'];
+        $_SESSION['construction_year'] = '1998';
+        $_SESSION['street'] = $_POST['street'];
+        $_SESSION['house_number'] = $_POST['house_number'];
+        $_SESSION['zip'] = $_POST['zip'];
+        $_SESSION['city'] = $_POST['city'];
+        $_SESSION['country'] = $_POST['country'];
+        $_SESSION['is_apartment'] = isset($_POST["is_apartment"]) ? 1 : 0;
+        $_SESSION['is_for_rent'] = isset($_POST["is_for_rent"]) ? 1 : 0;
+        $_SESSION['has_basement'] = isset($_POST["has_basement"]) ? 1 : 0;
+        $_SESSION['has_garden'] = isset($_POST["has_garden"]) ? 1 : 0;
+        $_SESSION['has_balcony'] = isset($_POST["has_balcony"]) ? 1 : 0;
+        $_SESSION['has_bathtub'] = isset($_POST["has_bathtub"]) ? 1 : 0;
+        $_SESSION['has_elevator'] = isset($_POST["has_elevator"]) ? 1 : 0;
+    }
+
+    function checkAllPostVariablesAreSet() {
+        if (($_POST['offer_name'] != '') and
+            (isset($_POST['is_apartment'])) and
+            (isset($_POST['is_for_rent'])) and
+            ($_POST['number_of_rooms'] != '') and
+            ($_POST['qm'] != '') and
+            ($_POST['street'] != '') and
+            ($_POST['house_number'] != '') and
+            ($_POST['zip'] != '') and
+            ($_POST['city'] != '') and
+            ($_POST['country'] != '')
+        ) {
+            return true;
+        }
+        return false;
+    }
 
 ?>
 
