fc61d5750fcccf365240106f92f178a3bc742ea5
Oliver <oliver@buettner-hh.de>
2019-05-19 20:37:57 +0200

    adjusted login + signup for new DB-Structure and extracted login-page css to own file

--- includes/functions/login.php
+++ includes/functions/login.php
@@ -6,16 +6,16 @@
         $password = $_POST['password'];
         
         //executing SQL
-        $login_statement = pdo()->prepare("SELECT account_id, password FROM account WHERE email = :email");
-        $result = $login_statement->execute(array('email' => $email));
+        $login_statement = pdo()->prepare("SELECT acc_id, acc_password FROM account WHERE acc_email = :acc_email");
+        $result = $login_statement->execute(array('acc_email' => $email));
         $account = $login_statement->fetch();
 
         //checks if password and email aren't empty
         if($password && $email){
             
             //checks if the password is the same as hash-password in DB
-            if (password_verify($password, $account['password'])) {
-                $_SESSION['account_id'] = $account['account_id'];
+            if (password_verify($password, $account['acc_password'])) {
+                $_SESSION['acc_id'] = $account['acc_id'];
                 header("Location: /?logged_in=true");
                 die();
             } 
