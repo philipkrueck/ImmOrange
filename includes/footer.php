2e5acddf37496a84f89ec6e708e4a88729de8b32
Oliver <oliver@buettner-hh.de>
2019-05-28 15:19:46 +0200

    changed relative paths to absolute paths

--- includes/footer.php
+++ includes/footer.php
@@ -4,10 +4,10 @@
       <div class="smallfooter">
 
         <h4><i>Kategorien:</i></h4>
-        <a href="/pages/category.php?category=houses"><p style="text-align: center; margin-left: 0px"> Häuser  <img class="iconHouse" src="../img/icons/house.png"></p></a>
-        <a href="/pages/category.php?category=apartments"><p style="text-align: center; margin-left: -32px"> Wohnungen   <img class="iconHouse" src="../img/icons/apartement.png"></p></a>
-        <a href="/pages/category.php?category=hamburg"><p style="text-align: center; margin-left: -30px"> in Hamburg  <img class="iconHouse" src="../img/icons/location.png" ></p></a>
-        <a href="/pages/category.php?category=berlin"><p style="text-align: center; margin-left: -5px"> in Berlin  <img class="iconHouse" src="../img/icons/location.png" ></p></a>
+        <a href="/pages/category.php?category=houses"><p style="text-align: center; margin-left: 0px"> Häuser  <img class="iconHouse" src="/img/icons/house.png"></p></a>
+        <a href="/pages/category.php?category=apartments"><p style="text-align: center; margin-left: -32px"> Wohnungen   <img class="iconHouse" src="/img/icons/apartement.png"></p></a>
+        <a href="/pages/category.php?category=hamburg"><p style="text-align: center; margin-left: -30px"> in Hamburg  <img class="iconHouse" src="/img/icons/location.png" ></p></a>
+        <a href="/pages/category.php?category=berlin"><p style="text-align: center; margin-left: -5px"> in Berlin  <img class="iconHouse" src="/img/icons/location.png" ></p></a>
 
       </div>
 
@@ -15,9 +15,9 @@
       <div class="smallfooter">
 
         <h3>Finde uns auf:</h3>
-        <a href="https://www.facebook.com/"><img class="iconTIF" src="../img/icons/Facebook.png"></a>
-        <a href="https://twitter.com/?lang=de"><img class="iconTIF" src="../img/icons/Twitter.png"></a>
-        <a href="https://www.instagram.com/?hl=de"><img class="iconTIF" src="../img/icons/instagram.png"></a>
+        <a href="https://www.facebook.com/"><img class="iconTIF" src="/img/icons/Facebook.png"></a>
+        <a href="https://twitter.com/?lang=de"><img class="iconTIF" src="/img/icons/Twitter.png"></a>
+        <a href="https://www.instagram.com/?hl=de"><img class="iconTIF" src="/img/icons/instagram.png"></a>
 
       </div>
 
@@ -27,16 +27,16 @@
         <h3>Lerne die Entwickler kennen:</h3>
         <div class="developer">
           <div>
-            <p style="text-align: center; margin-right: 5px">  <img class="bitmoji" src="../img/icons/phillippbitmoji.png">Phillip </p>
+            <p style="text-align: center; margin-right: 5px">  <img class="bitmoji" src="/img/icons/phillippbitmoji.png">Phillip </p>
           </div>
           <div>
-            <p style="text-align: center; margin-right: 6.5px">  <img class="bitmoji" src="../img/icons/ollibitmoji.png">Oliver </p>
+            <p style="text-align: center; margin-right: 6.5px">  <img class="bitmoji" src="/img/icons/ollibitmoji.png">Oliver </p>
           </div>
           <div>
-            <p style="text-align: center; margin-right: 21px">  <img class="bitmojileo" src="../img/icons/bitmoji.png">Leo </p>
+            <p style="text-align: center; margin-right: 21px">  <img class="bitmojileo" src="/img/icons/bitmoji.png">Leo </p>
           </div>
           <div>
-            <p style="text-align: center; margin-right: 0px">  <img class="bitmoji" src="../img/icons/marcelbitmoji.png">Marcel </p>
+            <p style="text-align: center; margin-right: 0px">  <img class="bitmoji" src="/img/icons/marcelbitmoji.png">Marcel </p>
           </div>
         </div>
       </div>
